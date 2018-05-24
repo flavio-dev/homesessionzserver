8dea80afcb89fad8f026bab90ec98e0e55667dd1
Flavian Buteau <flavian.buteau@affinitas.de>
2018-05-24 11:24:23 +0200

    testing

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 
 // Priority serve any static files.
@@ -6,13 +7,20 @@ const PORT = Number(process.env.PORT || 4000);
 
 // Answer API requests.
 app.get('/users', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  res.send(JSON.stringify({
-    test: 'hello world'
-  }));
+
+  request('https://api.mixcloud.com/NetilRadio/calming-waves-wfrancesco/', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.set('Content-Type', 'application/json');
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    // res.send(JSON.stringify({
+    //   test: 'hello world'
+    // }));
+    res.send(body)
+  });
+
 });
 
 app.listen(PORT, function () {
