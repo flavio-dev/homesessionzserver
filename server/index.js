be9bab67a7543d38ea4b75197604b07a4b8a2f43
flavio-dev <trash_flavio@hotmail.fr>
2018-05-26 17:49:15 +0200

    new dev on server - url check on origin, picking cloudacst, user etc...

--- server/index.js
+++ server/index.js
@@ -6,23 +6,27 @@ const app = express();
 const PORT = Number(process.env.PORT || 4000);
 
 // Answer API requests.
-app.get('/users', function (req, res) {
-
-  request('https://api.mixcloud.com/NetilRadio/calming-waves-wfrancesco/', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred and handle it
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    res.set('Content-Type', 'application/json');
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
-    // res.send(JSON.stringify({
-    //   test: 'hello world'
-    // }));
-    res.send(body)
-  });
-
+app.get('/cloudcast/:user/:cloudcastKey/:embedHtml*?', function (req, res) {
+    if (req.headers.origin !== 'http://localhost:3000' && typeof(req.headers.origin) !== 'undefined') {
+      res.status(400);
+      res.send('None shall pass');
+    } else {
+        let url = 'https://api.mixcloud.com/' + req.params.user + '/' + req.params.cloudcastKey
+        if (req.params.embedHtml === 'embed-html') {
+            url = url + '/embed-html'
+        }
+        request(encodeURI(url), function (error, response, body) {
+            console.log('error:', error); // Print the error if one occurred and handle it
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            res.set('Content-Type', 'application/json');
+            res.set('Access-Control-Allow-Origin', '*');
+            res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+            res.set('Access-Control-Allow-Headers', 'Content-Type');
+            res.send(body)
+        });
+    }
 });
 
 app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
