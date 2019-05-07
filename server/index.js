62710fe968d6e81c5c0bd993b95fc5b8acb673c6
Flavian Buteau <flavian.buteau@affinitas.de>
2019-05-07 16:45:57 +0200

    test with jsdom.. the sections still not loaded even with some delay for script executions

--- server/index.js
+++ server/index.js
@@ -1,12 +1,82 @@
+'use strict';
 const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
+const { JSDOM } = require('jsdom');
 const app = express();
 
 // Priority serve any static files.
 const PORT = Number(process.env.PORT || 4000);
 
 // Answer API requests.
+// app.get('/cloudcast/extrainfo/:user/:cloudcastKey', function (req, res) {
+//     if (
+//       (req.headers.origin !== 'https://localhost:3000' &&
+//       req.headers.origin !== 'https://www.homesessio.nz' &&
+//       req.headers.origin !== 'https://homesessio.nz') ||
+//       typeof(req.headers.origin) === 'undefined'
+//     ) {
+//       res.status(400);
+//       res.send('None shall pass');
+//     } else {
+//         let url = 'https://www.mixcloud.com/' + req.params.user + '/' + req.params.cloudcastKey
+//         request(encodeURI(url), function (error, response, body) {
+//             console.log('error:', error); // Print the error if one occurred and handle it
+//             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//             let featuringArtistList = []
+//             let previewUrl = ''
+//
+//             if (!error) {
+//               const $ = cheerio.load(body);
+//               const datas = $('#relay-data');
+//               let dataWeNeed = [];
+//               if (datas[0] && datas[0].children[0]) {
+//                 dataWeNeed = JSON.parse(datas[0].children[0].data.replace(/&quot;/g,'"'));
+//               }
+//               // let sections = []
+//               for (let data of dataWeNeed) {
+//                 if(data.hasOwnProperty('cloudcast')) {
+//                   try {
+//                       var featuringArtistListTemp = data.cloudcast.data.cloudcastLookup.featuringArtistList;
+//                       var previewUrlTemp = data.cloudcast.data.cloudcastLookup.previewUrl;
+//                       // var sectionsTemp = data.cloudcast.data.cloudcastLookup.sections;
+//                       // console.log('data.cloudcast.data.cloudcastLookup = ', data.cloudcast.data.cloudcastLookup);
+//                   } catch ($error){
+//
+//                   }
+//                   if(featuringArtistListTemp){
+//                       featuringArtistList = featuringArtistListTemp;
+//                   }
+//
+//                   if(previewUrlTemp){
+//                       previewUrl = previewUrlTemp;
+//                   }
+//
+//                   // if(sectionsTemp){
+//                   //     sections = sectionsTemp;
+//                   // }
+//
+//                   if (featuringArtistList.length && previewUrl.length) {
+//                     break;
+//                   }
+//                 }
+//               }
+//             }
+//
+//             const returnedData = {
+//               featuringArtistList,
+//               previewUrl
+//             }
+//
+//             res.set('Content-Type', 'application/json');
+//             res.set('Access-Control-Allow-Origin', '*');
+//             res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+//             res.set('Access-Control-Allow-Headers', 'Content-Type');
+//             res.send(returnedData)
+//         });
+//     }
+// });
+
 app.get('/cloudcast/extrainfo/:user/:cloudcastKey', function (req, res) {
     if (
       (req.headers.origin !== 'https://localhost:3000' &&
@@ -19,51 +89,88 @@ app.get('/cloudcast/extrainfo/:user/:cloudcastKey', function (req, res) {
     } else {
         let url = 'https://www.mixcloud.com/' + req.params.user + '/' + req.params.cloudcastKey
         request(encodeURI(url), function (error, response, body) {
+            const options = {
+              resources: 'usable',
+              runScripts: 'dangerously',
+            };
             console.log('error:', error); // Print the error if one occurred and handle it
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             let featuringArtistList = []
             let previewUrl = ''
+            let sections = []
 
             if (!error) {
-              const $ = cheerio.load(body);
-              const datas = $('#relay-data');
-              let dataWeNeed = [];
-              if (datas[0] && datas[0].children[0]) {
-                dataWeNeed = JSON.parse(datas[0].children[0].data.replace(/&quot;/g,'"'));
+              // const { document } = (new JSDOM(body, options)).window;
+              JSDOM.fromURL(url, options).then((dom)=>{
+                  let dataWeNeed = [];
+                  setTimeout(() => {
+                      const datas = dom.window.document.querySelector('#relay-data');
+                      dataWeNeed = JSON.parse(datas.innerHTML.replace(/&quot;/g,'"'));
+                      for (let data of dataWeNeed) {
+                        if(data.hasOwnProperty('cloudcast')) {
+                          try {
+                              var featuringArtistListTemp = data.cloudcast.data.cloudcastLookup.featuringArtistList;
+                              var previewUrlTemp = data.cloudcast.data.cloudcastLookup.previewUrl;
+                              var sectionsTemp = data.cloudcast.data.cloudcastLookup.sections;
+                              // console.log("@@@ featuringArtistListTemp = ", featuringArtistListTemp);
+                              // console.log("@@@ previewUrlTemp = ", previewUrlTemp);
+                              if (sectionsTemp.length) {
+                                console.log("@@@ sectionsTemp = ", sectionsTemp);
+                              }
+                              // console.log('data.cloudcast.data.cloudcastLookup = ', data.cloudcast.data.cloudcastLookup);
+                          } catch ($error){
+
+                          }
+                        }
+                      }
+                  }, 10000);
+                });
               }
-              // let sections = []
-              for (let data of dataWeNeed) {
-                if(data.hasOwnProperty('cloudcast')) {
-                  try {
-                      var featuringArtistListTemp = data.cloudcast.data.cloudcastLookup.featuringArtistList;
-                      var previewUrlTemp = data.cloudcast.data.cloudcastLookup.previewUrl;
-                      // var sectionsTemp = data.cloudcast.data.cloudcastLookup.sections;
-                      // console.log('data.cloudcast.data.cloudcastLookup = ', data.cloudcast.data.cloudcastLookup);
-                  } catch ($error){
 
-                  }
-                  if(featuringArtistListTemp){
-                      featuringArtistList = featuringArtistListTemp;
-                  }
-
-                  if(previewUrlTemp){
-                      previewUrl = previewUrlTemp;
-                  }
-
-                  // if(sectionsTemp){
-                  //     sections = sectionsTemp;
-                  // }
-
-                  if (featuringArtistList.length && previewUrl.length) {
-                    break;
-                  }
-                }
-              }
-            }
+            //   const datas = document.querySelector('#relay-data');
+            //   console.log('#### datas.innerHTML = ', datas.innerHTML);
+            //   let dataWeNeed = [];
+            //   if (datas.innerHTML && datas.innerHTML.length) {
+            //     dataWeNeed = JSON.parse(datas.innerHTML.replace(/&quot;/g,'"'));
+            //   }
+            //   console.log('####dataWeNeed = ', dataWeNeed);
+            //   // let sections = []
+            //   for (let data of dataWeNeed) {
+            //     if(data.hasOwnProperty('cloudcast')) {
+            //       try {
+            //           var featuringArtistListTemp = data.cloudcast.data.cloudcastLookup.featuringArtistList;
+            //           var previewUrlTemp = data.cloudcast.data.cloudcastLookup.previewUrl;
+            //           var sectionsTemp = data.cloudcast.data.cloudcastLookup.sections;
+            //           // console.log('data.cloudcast.data.cloudcastLookup = ', data.cloudcast.data.cloudcastLookup);
+            //       } catch ($error){
+            //
+            //       }
+            //       console.log('###### data.cloudcast.data.cloudcastLookup = ', data.cloudcast.data.cloudcastLookup);
+            //       if(featuringArtistListTemp){
+            //           featuringArtistList = featuringArtistListTemp;
+            //       }
+            //
+            //       if(previewUrlTemp){
+            //           previewUrl = previewUrlTemp;
+            //       }
+            //
+            //       if(sectionsTemp){
+            //           sections = sectionsTemp;
+            //       }
+            //
+            //       if (featuringArtistList.length && previewUrl.length && sections.length) {
+            //         break;
+            //       }
+            //     }
+            //   }
+            // }
+            //
+            // console.log('#### sections = ', sections);
 
             const returnedData = {
               featuringArtistList,
-              previewUrl
+              previewUrl,
+              sections
             }
 
             res.set('Content-Type', 'application/json');
